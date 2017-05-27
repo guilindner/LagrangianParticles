bd672d8966a0ca5646019cf39321a75fa21b0d67
Guilherme Lindner <lindner.guilherme@gmail.com>
2017-05-27 20:25:37 +0200

    last changes

--- load.py
+++ load.py
@@ -19,27 +19,30 @@ data = np.zeros(time)
 posx = np.zeros(time)
 posy = np.zeros(time)
 posz = np.zeros(time)
+posx2 = np.zeros(time)
+posy2 = np.zeros(time)
+posz2 = np.zeros(time)
+posx3 = np.zeros(time)
+posy3 = np.zeros(time)
+posz3 = np.zeros(time)
 
 u_rms = np.mean(vx)
-u_fluc = vx[110,:] - u_rms
-
-nech = 250
-nup = 1000
-Ruu = np.zeros(nech)
+#u_fluc = vx[110,:] - u_rms
+u_fluc = vx
 #velx = np.mean(vx,axis=1)
 velx = vx[:,0]
+sigma_u = np.std(vx)
 #acelx = np.mean(Dvx,axis=1)
 acelx = Dvx[:,0]
 print(velx.size)
-for i in range(1,nech):
-    for j in range(1, nup):
-        Ruu[i] += (velx[j]* velx[j+i])/nup
-Li = np.sum(Ruu)/(3*np.var(velx))
+corrx = np.corrcoef(vx)
+deltaT = 0.004
+Li = np.sum(np.mean(corrx,axis=0))/(3*np.var(vx))*deltaT
 print('Integral scale:', Li)
-Ti = Li/np.std(velx)
+Ti = Li/sigma_u
 print('Integral time:', Ti)
 A = 0.5 #0.5 - 1.0
-epsilon = A*np.std(velx)**3/Li
+epsilon = A*np.std(vx)**3/Li
 print('Epsilon:',epsilon)
 nu = 1.5e-5 #viscosity
 Re_L = np.std(velx)*Li/nu
@@ -50,8 +53,10 @@ tau_l = np.sqrt(2*np.mean(u_fluc**2)/np.mean(acelx**2))
 print('tau_l ???', tau_l)
 tau_eta = (nu/epsilon)**0.5
 print('Kolmogorov time scale', tau_eta)
-#taylor = (15*u_fluc/epsilon)**0.5 * u_fluc
-#Re_taylor = 1
+taylor = (15*nu/epsilon)**0.5 * sigma_u
+print('Taylor microscale:',taylor)
+Re_taylor = sigma_u*taylor/nu
+print('Reynolds Taylor',Re_taylor)
 Te = tau_l/u_rms #large-eddy turnover time
 print('Large-eddy turnover time', Te)
 
@@ -64,18 +69,31 @@ if sys.argv[1] == 'time':
         t = i
         ui = (vx[0,:])/np.std(vx[0,:])
         uit = (vx[0+t,:])/np.std(vx[0+t,:])
-        Rii = np.mean(ui*uit)/(np.mean(ui**2)**0.5 * np.mean(uit**2)**0.5)
+        Rii = np.mean(ui*uit)/np.var(vx[0,:])#(np.mean(ui**2)**0.5 * np.mean(uit**2)**0.5)
         Tl[i] = Rii
     print(Tl)
+    print('area',(np.sum(Rii))/np.var(vx[0,:]))
     fig, ax = plt.subplots()
+    #ax.set_xscale('log')
     ax.plot(Tl)
     plt.show()
 
+if sys.argv[1] == 'time2':
+    Tl = np.zeros(nech)
+    for i in range(1,nech):
+        for j in range(1, nup):
+            Tl[i] += (velx[j]* velx[j+i])/nup
+    print(Tl)
+    print('area',(np.sum(Tl))/np.var(velx))
+    fig, ax = plt.subplots()
+    #ax.set_xscale('log')
+    ax.plot(Tl)
+    plt.show()
 
 if sys.argv[1] == 'second':
     nech = 250
     nup = 1000
-    x = np.arange(0,250)/tau_eta
+    x = np.arange(0,250)#/tau_eta
     s2u = np.zeros(nech)
     s2u2 = np.zeros(nech)
     for i in range(1,nech):
@@ -90,16 +108,16 @@ if sys.argv[1] == 'second':
     plt.show()
 
 if sys.argv[1] == 'spectrum':
-    for i in range(100):
+    for i in range(500):
         for j in range(0,time):
             data[j] += (vx[j][i]**2+vy[j][i]**2+vz[j][i]**2)**0.5
     data = data/100.
-    fs = 250
+    fs = 20000
     f, Pxx= scipy.signal.welch(data, fs, window='hanning', nperseg=1024, 
     noverlap=16 , nfft=None, detrend='constant', return_onesided=True, 
     scaling='density', axis=-1)
-    m = np.arange(1.,10.)
-    n = 1*m**(-2.)
+    m = np.arange(100.,1000.)
+    n = 300*m**(-2.)
 
     plt.figure()
     plt.loglog(f, np.sqrt(Pxx))
@@ -111,12 +129,19 @@ if sys.argv[1] == 'spectrum':
 elif sys.argv[1] == 'trace': 
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    for i in range(1):
-        for j in range(0,time):
-            posx[j] = x[j][i]
-            posy[j] = y[j][i]
-            posz[j] = z[j][i]
-        ax.plot(posx,posy,posz,label='particle{}'.format(i))
+    for j in range(0,time):
+        posx[j] = x[j][55]
+        posy[j] = y[j][55]
+        posz[j] = z[j][55]
+        posx2[j] = x[j][133]
+        posy2[j] = y[j][133]
+        posz2[j] = z[j][133]
+        posx3[j] = x[j][433]
+        posy3[j] = y[j][433]
+        posz3[j] = z[j][433]
+    ax.plot(posx,posy,posz,label='particle 1')
+    ax.plot(posx2,posy2,posz2,label='particle 2')
+    ax.plot(posx3,posy3,posz3,label='particle 3')
     ax.legend()    
     plt.show()
 elif sys.argv[1] == 'spdf':
@@ -159,11 +184,6 @@ elif sys.argv[1] == 'together':
     x = np.linspace(-sigma*3,sigma*3,data.size)
     y = norm.logpdf(x, mu, sigma)
     ax.plot(x, y, 'r--', linewidth=2)
-    #m = plt.plot()
-    #plot
-    #ax.xlabel('Smarts')
-    #ax.ylabel('Probability')
-    #ax.title(r'$\mathrm{Histogram\ of\ vx:}\ \mu=%.3f,\ \sigma=%.3f$' %(mu, sigma))
     ax.grid(which="both")
     #ax.set_yscale('log')
     
@@ -181,7 +201,7 @@ elif sys.argv[1] == 'tpdf':
         t = 10*(i+1)
         stvx = vx[0,:] - vx[t,:]
         x = (stvx)/np.sqrt(np.mean((stvx)**2))
-        y = norm.logpdf(stvx)/t
+        y = norm.pdf(stvx)/t
         ax.scatter(x,y)
     #ax.scatter(x,norm.logpdf(stvx))
     #ax.set_ylim(-8,0)
@@ -250,6 +270,20 @@ elif sys.argv[1] == 'vpdf':
     y = norm.pdf(bins, mu, sigma)
     ax.plot(bins, y, 'r--', linewidth=2)
     ax.set_xlim(-4,4)
+    ax.set_ylabel('PDF')
+    ax.set_xlabel('velocity [m/s]')
+    ax.grid(which="both")
+    plt.show()
+
+elif sys.argv[1] == 'vpdf2':
+
+    fig, ax = plt.subplots()
+    data = np.sort(vx[100,:])
+    fit = norm.pdf(data, np.mean(data), np.std(data))  
+    ax.plot(data,fit,'-o')
+    ax.set_xlim(-4,4)
+    ax.set_ylabel('PDF')
+    ax.set_xlabel('velocity [m/s]')
     ax.grid(which="both")
     plt.show()
 
@@ -262,7 +296,18 @@ elif sys.argv[1] == 'apdf':
     ax.grid(which="both")
     plt.show()
 
+elif sys.argv[1] == 'apdf2':
 
+    fig, ax = plt.subplots()
+    data = np.sort(Dvx[100,:])
+    fit = norm.pdf(data, np.mean(data), np.std(data))
+    print(fit)
+    ax.plot(data/np.std(data),fit,'-o')
+    ax.set_xlim(-10,10)
+    ax.set_ylabel('PDF')
+    ax.set_xlabel('a/<a^2>^0.5')
+    ax.grid(which="both")
+    plt.show()
 
 else:
     print('Choose one: spectrum, trace, pdf, fluc, ...')
